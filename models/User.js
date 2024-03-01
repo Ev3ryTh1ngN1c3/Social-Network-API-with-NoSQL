c509c2153a4a8076d0e733adc49bb1028620e724
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-02-29 22:07:18 -0500

    implemented User schema with userName, email, thoughts, and friends fields, along with virtual property 'friendCount' to calculate the number of friends. Excluded 'id' field from JSON output & defined schema settings. Created User model & exported it

---
+++ models/User.js
@@ -0,0 +1,54 @@
+ // Import necessary modules from Mongoose
+const { Schema, model } = require('mongoose');
+
+// Define the User Schema
+const userSchema = new Schema(
+  {
+    // Define the userName field with specific properties
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    // Define the email field with specific properties including email validation
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    // Define the thoughts field as an array of ObjectIds referencing the 'thought' model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      }
+    ],
+    // Define the friends field as an array of ObjectIds referencing the 'user' model (self-reference)
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      }
+    ]
+  },
+  {
+    // Define schema settings
+    toJSON: {
+      virtuals: true, // Include virtual properties when converting to JSON
+    },
+    id: false, // Exclude the 'id' field from the schema
+  }
+);
+
+// Define a virtual property 'friendCount' to calculate the number of friends a user has
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+// Create the User model based on the User Schema
+const User = model('User', userSchema);
+
+// Export the User model
+module.exports = User;
